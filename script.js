9926d293940617139d8e2415fe686dd339d0119b
larisaa07 <larisa_aveti@yahoo.com>
2024-10-29 11:55:39 -0400

    im so lost

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-let pageCounter = 1
-const btn = $('#btn')
+$( document ).ready(function() {
+    console.log( "ready!" );
+            
+    let btn = $('#submitDay');
+    btn.on('click', function(){
 
-btn.on('click', function() {
-    $.ajax({
-        type: 'GET',
-        url: `https://api.npoint.io/981011375e51b80de040`,
-        success: function (data) {
-            renderHTML(data)
-            pageCounter++
-            if (pageCounter > 3) {
-                btn.addClass('hide-me')
-            }
-        },
-        error: function () {
-            console.log('Connection Error')
+        let selectedDay = $('#dayInput').text();
+        if (selectedDay != ["A", "B", "C", "D", "E", "F", "G"]){
+            //add an error message
         }
-    });
-})
+
+    btn.on('click', function() {
+        $.ajax({
+            type: 'GET',
+            url: 'https://api.npoint.io/981011375e51b80de040',
+            dataType: 'json',
+            success: function (data) {
+                let 
+            },
+            error: function () {
+                console.log('Connection Error')
+            }
+        });
+    })
+    })
+    
+});
